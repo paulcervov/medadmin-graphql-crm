290a980559ef1a2870ad8317970669c9d86d600b
Paul Cervov <paulcervov@gmail.com>
2020-10-30 21:45:05 +0300

    Add searchQuery input to EmployerList interface

--- src/components/Employer/List/List.js
+++ src/components/Employer/List/List.js
@@ -2,14 +2,22 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import EmployerTable from "./Table/Table";
 
-function List({employers, onClickLoadMore, canClickLoadMore}) {
+function List({employers, searchQuery, onInputSearchQuery, onClickLoadMore, canClickLoadMore}) {
     return <div className="border pl-sm-4 pr-sm-4 pb-sm-3 pt-sm-3">
 
             <h2 className="border-bottom pb-sm-2 mb-sm-4">Список сотрудников</h2>
 
             <div className="row mb-sm-3">
 
-                <div className="col-sm-auto"></div>
+                <div className="col-sm-4">
+                    <input
+                        className="form-control"
+                        placeholder="Введите текст"
+                        autoComplete="off"
+                        onInput={onInputSearchQuery}
+                        value={searchQuery}
+                    />
+                </div>
 
                 <div className="col-sm-auto ml-sm-auto">
                     <Link className="btn btn-primary" to="/employers/create">Новый сотрудник</Link>
--- src/containers/Employer/List.js
+++ src/containers/Employer/List.js
@@ -1,10 +1,7 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import EmployerList from "../../components/Employer/List/List";
 
-import {
-    gql,
-    useQuery
-} from "@apollo/client";
+import {gql, useQuery} from "@apollo/client";
 
 const FIND_EMPLOYERS = gql`
     query findEmployers($page: Int = 1, $trashed: Trashed = WITHOUT) {
@@ -32,14 +29,22 @@ const FIND_EMPLOYERS = gql`
 `;
 
 function List() {
-    const {data, loading, error, fetchMore} = useQuery(FIND_EMPLOYERS, {variables: {trashed: 'WITH'}});
+    const isInitialMount = useRef(true);
+    const [searchQuery, setSearchQuery] = useState('');
+    const {data, loading, error, fetchMore, refetch} = useQuery(FIND_EMPLOYERS, {
+        variables: {
+            trashed: 'WITH',
+            searchQuery
+        },
+        pollInterval: (1000 * 60),
+    });
 
     async function onClickLoadMore() {
         await fetchMore({
             variables: {
                 page: data.findEmployers.paginatorInfo.currentPage + 1
             },
-            updateQuery: (prev, { fetchMoreResult }) => {
+            updateQuery: (prev, {fetchMoreResult}) => {
                 if (!fetchMoreResult) return prev;
                 return Object.assign({}, prev, {
                     findEmployers: {
@@ -50,6 +55,19 @@ function List() {
             }
         })
     }
+
+    async function onInputSearchQuery(event) {
+        setSearchQuery(event.target.value);
+    }
+
+    useEffect(() => {
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+        } else {
+            refetch();
+        }
+
+    }, [searchQuery])
 
     if (loading) {
         return <div className="alert alert-secondary">Загрузка...</div>;
@@ -66,6 +84,8 @@ function List() {
 
     return <EmployerList
         employers={data.findEmployers.data}
+        onInputSearchQuery={onInputSearchQuery}
+        searchQuery={searchQuery}
         onClickLoadMore={onClickLoadMore}
         canClickLoadMore={data.findEmployers.paginatorInfo.hasMorePages}
     />;
