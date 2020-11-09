04e358b4f19007dc10a547de7c06361be2ee8737
Paul Cervov <paulcervov@gmail.com>
2020-11-09 18:05:04 +0300

    Fixup import serviceWorker.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {ApolloClient, InMemoryCache, ApolloProvider,} from "@apollo/client";
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const client = new ApolloClient({
     uri: process.env.REACT_APP_API_URL,
